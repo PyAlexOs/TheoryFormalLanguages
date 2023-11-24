ddb652f04bd8db31a19199ee0ab32d5566fd0ccc
pyalexos <aligos@yandex.ru>
2023-11-24 08:55:16 +0300

    first 3 practices

---
+++ 1-2PR.py
@@ -0,0 +1,98 @@
+def transform(input: list, params: dict = {}) -> list:
+    signs = {"*": 3, "/": 3, "+": 2, "-": 2, "(": 1}
+    stack = list()
+    output = list()
+
+    for symbol in input:
+        if symbol == " ":
+            continue
+
+        symbol = symbol.strip()
+        if symbol.isdigit() or symbol in params.keys():
+            output.append(symbol)
+
+        elif symbol == "(":
+            stack.append(symbol)
+
+        elif symbol == ")":
+            temp = stack.pop()
+            while temp != "(":
+                output.append(temp)
+                temp = stack.pop()
+
+        elif symbol in signs.keys():
+            while len(stack) != 0 and signs[stack[-1]] >= signs[symbol]:
+                output.append(stack.pop())
+
+            stack.append(symbol)
+
+        else:
+            print(f"Can't parse symbol while transforming: {symbol}.")
+            exit(0)
+
+    while len(stack) != 0:
+        output.append(stack.pop())
+
+    return output
+
+
+def count_res(input: list, params: dict = {}) -> float:
+    stack = list()
+    for symbol in input:
+        if symbol.isdigit() or symbol in params.keys():
+            stack.append(symbol)
+
+        elif symbol in ["*", "/", "+", "-"]:
+            a = stack.pop()
+            b = stack.pop()
+
+            if a.isalpha():
+                a = params[a]
+            if b.isalpha():
+                b = params[b]
+
+            stack.append(str(eval(b + symbol + a)))
+
+        else:
+            print(f"Can't parse symbol while counting: {symbol}.")
+            exit(0)
+
+    return float(stack.pop())
+
+
+def prepare(string: str) -> list:
+    substr = ""
+    in_list = list()
+
+    for symbol in string:
+        if symbol in ["*", "/", "+", "-", "(", ")"]:
+            if len(substr) != 0:
+                in_list.append(substr)
+
+            in_list.append(symbol)
+            substr = ""
+
+        else:
+            substr += symbol
+
+    if len(substr) != 0:
+        in_list.append(substr)
+
+    return in_list
+
+
+if __name__ == '__main__':
+    expression = input('Input your equation: ')
+    expression = prepare(expression)
+    print('enter the values of all variables that participate in the expression (then enter "\\)')
+
+    params = {}
+    # params = {'a': '2', 'b': '3'}
+    key = input('Enter variable: ')
+    while key != "\\":
+        params[key] = input('Enter value: ')
+        key = input('Enter variable: ')
+
+    result = transform(expression, params)
+    print(' '.join(result))
+    print(count_res(result, params))
---
+++ 3PR.py
@@ -0,0 +1,88 @@
+from enum import Enum
+from os import path, SEEK_SET
+import re
+
+
+def tokenize(filepath: str) -> dict:
+    buffer: str
+    states = Enum('state', ['h', 'id', 'nm', 'dlm', 'asgn', 'err'])
+    tokens = {
+        'KEYWORDS': [],
+        'DELIMITERS': [],
+        'ASSIGN': [],
+        'IDs': [],
+        'NUMBERS': [],
+        'UNKNOWN': []
+    }
+
+    with open(filepath, 'r') as file:
+        current_state = states['h']
+        while symbol := file.read(1):
+
+            match current_state.name:
+                case 'h':
+                    while symbol in [' ', '\t', '\n']:
+                        symbol = file.read(1)
+                    if symbol == ':':
+                        current_state = states['asgn']
+                    elif symbol.isalpha() or symbol == '_':
+                        current_state = states['id']
+                    elif symbol.isdigit() or symbol in ['-', '+', '.']:
+                        current_state = states['nm']
+                    elif symbol in ['(', ')', ';', '<', '>', '=']:
+                        current_state = states['dlm']
+                    buffer = symbol
+
+                case 'asgn':
+                    buffer += symbol
+                    if symbol == '=':
+                        tokens['ASSIGN'].append(buffer)
+                    else:
+                        tokens['UNKNOWN'].append(buffer)
+
+                    buffer = ''
+                    current_state = states['h']
+
+                case 'id':
+                    while symbol.isalnum() or symbol == '_':
+                        buffer += symbol
+                        symbol = file.read(1)
+
+                    if buffer in ['for', 'do']:
+                        tokens['KEYWORDS'].append(buffer)
+                    else:
+                        tokens['IDs'].append(buffer)
+
+                    file.seek(file.tell() - 1, SEEK_SET)
+                    current_state = states['h']
+
+                case 'nm':
+                    while symbol.isdigit() or symbol in ['.', 'e', 'E', '-', '+']:
+                        buffer += symbol
+                        symbol = file.read(1)
+
+                    if re.match('^(?!^\s*$)\d*(\.\d+)?((e|E)(-|\+)?\d+)?$', buffer):
+                        tokens['NUMBERS'].append(buffer)
+                    else:
+                        tokens['UNKNOWN'].append(buffer)
+
+                    file.seek(file.tell() - 1, SEEK_SET)
+                    current_state = states['h']
+
+                case 'dlm':
+                    tokens['DELIMITERS'].append(buffer)
+                    file.seek(file.tell() - 1, SEEK_SET)
+                    current_state = states['h']
+
+    return tokens
+
+
+if __name__ == "__main__":
+    filepath = path.abspath(input('Enter filepath: '))  # files/test.txt
+    if path.exists(filepath):
+        tokens = tokenize(filepath)
+    else:
+        exit(f"Path {filepath} doesn't exists.")
+
+    for (key, value) in tokens.items():
+        print(key, ":", value)
